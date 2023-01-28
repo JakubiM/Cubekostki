fd9a6fd9ba4ed6dd67d72417cabd53739c4d1b81
PureFour <daniel.baziak@gmail.com>
2023-01-28 14:20:54 +0100

    sockets code moved to another branch

--- client/App.tsx
+++ client/App.tsx
@@ -2,19 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Game from "./src/components/Game";
-import SocketInfo from "./src/components/SocketInfo";
-import SocketContextComponent from "./src/context/SocketContextComponent";
 
 export default function App() {
   return (
-    <SocketContextComponent>
-      <View style={styles.container}>
-        <SocketInfo />
-        <Text>Napisz kostki tutaj!</Text>
-        <Game />
-        <StatusBar style="auto" />
-      </View>
-    </SocketContextComponent>
+    <View style={styles.container}>
+      <Text>Napisz kostki tutaj!</Text>
+      <Game />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
